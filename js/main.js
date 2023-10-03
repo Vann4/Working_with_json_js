88bf5a3ef03ad4a51d757bae16b2db2c56eb8f98
Vann4 <1vetlan1@mail.ru>
2023-10-03 20:34:07 +0300

    Добавил тег ul, создал json-строку, распарсил json-строку в json-объект

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-let head_creation = document.createElement('header');
+let header_create = document.createElement('header');
 let body_search = document.querySelector('body');
-body_search.prepend(head_creation); //Добавление header`a в начало body
-
+body_search.prepend(header_create); //Добавление header`a в начало body
 let head_search = document.querySelector('header');
 
-let h1_create = document.createElement('h1')
+let h1_create = document.createElement('h1');
 h1_create.textContent = 'Работа с JSON';
 h1_create.setAttribute('style', 'text-align:center;');
-head_search.appendChild(h1_create);
+head_search.appendChild(h1_create); //Добавление h1 в конец
+
+let main_create = document.createElement('main');
+head_search.after(main_create); //Вставка узлов или строк после header`a.
+let main_search = document.querySelector('main');
+
+let ul_create = document.createElement('ul');
+main_search.appendChild(ul_create);
+let ul_search = document.querySelector('ul')
+
+let users_json = '{"id":1, "users": [{"name": "Давид", "age": 20}, {"name": "Пётр", "age": 35}]}';
+let parse_users_json = JSON.parse(users_json);
+console.log(parse_users_json)
