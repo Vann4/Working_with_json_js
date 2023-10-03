0205a8a6418815efd7b8bad15e0d55020e891745
Vann4 <1vetlan1@mail.ru>
2023-10-03 20:40:20 +0300

    Написал цикл для вывода данных из массива в json-объекте, создал li в котором идет вывод данных

--- js/main.js
+++ js/main.js
@@ -18,4 +18,11 @@ let ul_search = document.querySelector('ul')
 
 let users_json = '{"id":1, "users": [{"name": "Давид", "age": 20}, {"name": "Пётр", "age": 35}]}';
 let parse_users_json = JSON.parse(users_json);
-console.log(parse_users_json)
+
+for (let i=0; i<parse_users_json.users.length; i++){
+    let data_array = parse_users_json.users[i];
+    let li_create = document.createElement('li');
+    li_create.setAttribute('style', 'font-size: 30px; margin-bottom: 2%;');
+    li_create.textContent = `Имя: ${data_array.name}, возраст: ${data_array.age}`
+    ul_search.appendChild(li_create)
+}
